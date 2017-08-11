4829e726a19d5f67a15f73313552bcac45ca3bfd
Jelle Dekker <jelle.dekker@bizzomate.com>
2017-08-11 07:55:57 +0200

    Enabled resize function
    - Enabled the resize function again, but only if pageInstantiated =
    false
    - Converted all console.log that were not error messages to logger.debug
    - Replaced deprecated MxContext.setTrackEntity with MxContext.SetContext
    - Added checks on "this._active" in more places to ensure delay
    resulting from MF calls or long page loads does not trigger the widget
    to execute when the HTML is already destroyed

--- src/DataviewLoader/widget/DataviewLoader.js
+++ src/DataviewLoader/widget/DataviewLoader.js
@@ -30,231 +30,241 @@ define([
 
   "dojo/text!DataviewLoader/widget/template/DataviewLoader.html"
 ], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoStyle, dojoClass, dojoLang, dojoEvent, widgetTemplate) {
-    "use strict";
+  "use strict";
 
-    // Declare widget's prototype.
-    return declare("DataviewLoader.widget.DataviewLoader", [_WidgetBase, _TemplatedMixin], {
-        // _TemplatedMixin will create our dom node using this HTML template.
-        templateString: widgetTemplate,
+  // Declare widget's prototype.
+  return declare("DataviewLoader.widget.DataviewLoader", [_WidgetBase, _TemplatedMixin], {
+    // _TemplatedMixin will create our dom node using this HTML template.
+    templateString: widgetTemplate,
 
-        // DOM elements
-        divContent: null,
-        divLoader: null,        
+    // DOM elements
+    divContent: null,
+    divLoader: null,
 
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-        _handles: null,
-        _contextObj: null,
-        _loadingStarted: false,
-        _pageInitiated: false,
-        active: true,
+    // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+    _handles: null,
+    _contextObj: null,
+    _loadingStarted: false,
+    _pageInitiated: false,
+    _active: true,
 
-        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
-        constructor: function () {
-            //logger.debug(this.id + ".constructor");
-            this._handles = [];
-        },
+    // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+    constructor: function () {
+      logger.debug(this.id + ".constructor");
+      this._handles = [];
+    },
 
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
-        postCreate: function () {
-            logger.debug(this.id + ".postCreate");
-            
-            this._updateRendering();
-            this._setupEvents();
-        },
+    // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+    postCreate: function () {
+      logger.debug(this.id + ".postCreate");
 
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-        update: function (obj, callback) {
-            if(this._contextObj !== obj){
-                console.log(this.id + ".update on new object");
-                this._loadingStarted = false;
+      this._updateRendering();
+      this._setupEvents();
+    },
 
-                this._contextObj = obj;
-                this._resetSubscriptions();
-                this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
-            } else {
-                // The callback, coming from update, needs to be executed, to let the page know it finished rendering
-                this._executeCallback(callback, "update");
-            }
-        },
+    // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+    update: function (obj, callback) {
+      if (this._contextObj !== obj) {
+        logger.debug(this.id + ".update on new object");
+        this._loadingStarted = false;
+        this._pageShown = false;
 
-        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
-        enable: function () {
-            logger.debug(this.id + ".enable");
-        },
+        this._contextObj = obj;
+        this._resetSubscriptions();
+        this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
+      } else {
+        // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+        this._executeCallback(callback, "update");
+      }
+    },
 
-        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
-        disable: function () {
-            logger.debug(this.id + ".disable");
-        },
+    // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+    enable: function () {
+      logger.debug(this.id + ".enable");
+    },
 
-        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
-        resize: function (box) {
-            logger.debug(this.id + ".resize");
-            // TODO: How to handle tabs and conditional visibility
-//            if (this.domNode.offsetParent !== null) {
-//                this._loadAndShowcontent();
-//            }
-        },
+    // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+    disable: function () {
+      logger.debug(this.id + ".disable");
+    },
 
-        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
-        uninitialize: function () {
-            logger.debug(this.id + ".uninitialize");
-            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
-            this.active = false;
-        },
+    // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+    resize: function (box) {
+      logger.debug(this.id + ".resize");
+      if (this.domNode.offsetParent !== null && this._pageInitiated === false) {
+        this._loadAndShowcontent();
+      }
+    },
 
-        // We want to stop events on a mobile device
-        _stopBubblingEventOnMobile: function (e) {
-            logger.debug(this.id + "._stopBubblingEventOnMobile");
-            if (typeof document.ontouchstart !== "undefined") {
-                dojoEvent.stop(e);
-            }
-        },
+    // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+    uninitialize: function () {
+      logger.debug(this.id + ".uninitialize");
+      // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+      this._active = false;
+    },
 
-        // Attach events to HTML dom elements
-        _setupEvents: function () {
-            logger.debug(this.id + "._setupEvents");
-        },
+    // We want to stop events on a mobile device
+    _stopBubblingEventOnMobile: function (e) {
+      logger.debug(this.id + "._stopBubblingEventOnMobile");
+      if (typeof document.ontouchstart !== "undefined") {
+        dojoEvent.stop(e);
+      }
+    },
 
-        // Rerender the interface.
-        _updateRendering: function (callback) {
-            logger.debug(this.id + "._updateRendering");
+    // Attach events to HTML dom elements
+    _setupEvents: function () {
+      logger.debug(this.id + "._setupEvents");
+    },
 
-            if (this._contextObj) {
-                dojoStyle.set(this.divContent, "display", "none");
-                dojoStyle.set(this.divLoader, "display", "block");
-                
-                if(this.fadeContent){
-                    dojoClass.add(this.divContent, "loaderfade");
-                }
-            }
+    // Rerender the interface.
+    _updateRendering: function (callback) {
+      logger.debug(this.id + "._updateRendering");
 
-            // The callback, coming from update, needs to be executed, to let the page know it finished rendering
-            this._executeCallback(callback, "_updateRendering");
+      if (this._contextObj && this._active) {
+        dojoStyle.set(this.divContent, "display", "none");
+        dojoStyle.set(this.divLoader, "display", "block");
 
-            if (this.domNode.offsetParent !== null) {
-                this._loadAndShowcontent();
-            }
-        },
-
-        _loadAndShowcontent: function () {
-            logger.debug(this.id + "._loadAndShowcontent");
-            if(this._loadingStarted == false){
-                this._loadingStarted = true;
-                if (this._contextObj && this.loadingMF) {
-                    this._execMf(this.loadingMF, this._contextObj.getGuid(), this._processMicroflowCallback);
-                } else if (this._contextObj) {
-                    this._setPage(this._contextObj);
-                }
-            }
-        },
-
-        _processMicroflowCallback: function (objs) {
-            logger.debug(this.id + '._processMicroflowCallback');
-            if (this.active) {
-                if(this.asyncCall)
-                    this._setPage(this._contextObj);
-                else
-                    this._setPage(objs[0]);
-            } else {
-                 console.info(this.id + "._processMicroflowCallback Skip loading because widget is destroyed.");
-            }
-        },
-
-        _setPage: function (pageObj) {
-            logger.debug(this.id + '._setPage');
-
-            if(this._pageInitiated) {
-                if (this._loadingStarted) {
-                    this._showPage();
-                } else {
-                    console.log(this.id + "_setPage skip because already set.");
-                }
-            } else {
-                this._pageInitiated = true;
-                this.divContent.innerHTML= "";
-                console.log(this.id + "_setPage");
-                if (pageObj) {
-                    var pageContext = new mendix.lib.MxContext();
-                    pageContext.setTrackObject(pageObj);
-                    mx.ui.openForm(this.pageContent, {
-                        context: pageContext,
-                        location: "content",
-                        domNode: this.divContent,
-                        callback: dojoLang.hitch(this, this._showPage),
-                        error: function (error) {
-                            console.log(error.description);
-                        }
-                    });
-                } else {
-                    mx.ui.openForm(this.pageContent, {
-                        location: "content",
-                        domNode: this.divContent,
-                        callback: dojoLang.hitch(this, this._showPage),
-                        error: function (error) {
-                            console.log(error.description);
-                        }
-                    });
-                }
-            }
-        },
-
-        _showPage: function () {
-            console.log(this.id + "._showPage on form");
-            
-            dojoStyle.set(this.divContent, "display", "block");
-            dojoStyle.set(this.divLoader, "display", "none");
-            
-            this._loadingStarted = false;
-        },
-        
-        // Reset subscriptions.
-        _resetSubscriptions: function () {
-            logger.debug(this.id + "._resetSubscriptions");
-            // Release handles on previous object, if any.
-            this.unsubscribeAll();
-
-            // When a mendix object exists create subscribtions.
-            if (this._contextObj && this.refreshAction) {
-                logger.debug(this.id + "._resetSubscriptions setup refresh handler");
-                this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    callback: dojoLang.hitch(this, function (guid) {
-                        if(this._loadingStarted == false){
-                            console.log(this.id + ".Refresh triggered.");
-                            this._updateRendering();
-                        } else {
-                            console.log(this.id + ".Refresh skip because of loading started.");
-                        }
-                    })
-                });
-            }
-        },
-
-        _execMf: function (mf, guid, cb) {
-            logger.debug(this.id + "._execMf" + (mf ? ": " + mf : ""));
-            if (mf && guid) {
-                mx.ui.action(mf, {
-                    async : this.asyncCall,
-                    params: {
-                        applyto: "selection",
-                        guids: [guid]
-                    },
-                    callback: (cb && typeof cb === "function" ? dojoLang.hitch(this, cb) : null),
-                    error: function (error) {
-                        console.debug(error.description);
-                    }
-                }, this);
-            }
-        },
-
-        _executeCallback: function (cb, from) {
-            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
-            if (cb && typeof cb === "function") {
-                cb();
-            }
+        if (this.fadeContent) {
+          dojoClass.add(this.divContent, "loaderfade");
         }
-    });
+      }
+
+      // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+      this._executeCallback(callback, "_updateRendering");
+
+      if (this.domNode.offsetParent !== null) {
+        this._loadAndShowcontent();
+      }
+    },
+
+    _loadAndShowcontent: function () {
+      logger.debug(this.id + "._loadAndShowcontent");
+      if (this._loadingStarted == false && this._active) {
+        this._loadingStarted = true;
+        if (this._contextObj && this.loadingMF) {
+          this._execMf(this.loadingMF, this._contextObj.getGuid(), this._processMicroflowCallback);
+        } else if (this._contextObj) {
+          this._setPage(this._contextObj);
+        }
+      } else {
+        if (this._active == false){
+          logger.debug(this.id + "._loadAndShowcontent Skip loading because widget is destroyed.");
+        } else if (this._loadingStarted){
+          logger.debug(this.id + "._loadAndShowcontent Skip loading because loadingStarted.");
+        }
+        
+      }
+    },
+
+    _processMicroflowCallback: function (objs) {
+      logger.debug(this.id + '._processMicroflowCallback');
+      if (this._active) {
+        if (this.asyncCall)
+          this._setPage(this._contextObj);
+        else
+          this._setPage(objs[0]);
+      } else {
+        logger.debug(this.id + "._processMicroflowCallback Skip loading because widget is destroyed.");
+      }
+    },
+
+    _setPage: function (pageObj) {
+      logger.debug(this.id + '._setPage');
+
+      if (this._pageInitiated) {
+        if (this._loadingStarted) {
+          this._showPage();
+        } else {
+          logger.debug(this.id + "_setPage skip because already set.");
+        }
+      } else {
+        this._pageInitiated = true;
+        this.divContent.innerHTML = "";
+        if (pageObj) {
+          var pageContext = new mendix.lib.MxContext();
+          pageContext.setContext(pageObj.getEntity(), pageObj.getGuid());
+          mx.ui.openForm(this.pageContent, {
+            context: pageContext,
+            location: "content",
+            domNode: this.divContent,
+            callback: dojoLang.hitch(this, this._showPage),
+            error: function (error) {
+              console.log(error.description);
+            }
+          });
+        } else {
+          mx.ui.openForm(this.pageContent, {
+            location: "content",
+            domNode: this.divContent,
+            callback: dojoLang.hitch(this, this._showPage),
+            error: function (error) {
+              console.log(error.description);
+            }
+          });
+        }
+      }
+    },
+
+    _showPage: function () {
+      logger.debug(this.id + "._showPage on form");
+
+      if (this._active) {
+        dojoStyle.set(this.divContent, "display", "block");
+        dojoStyle.set(this.divLoader, "display", "none");
+      
+        this._loadingStarted = false;
+      }
+
+    },
+
+    // Reset subscriptions.
+    _resetSubscriptions: function () {
+      logger.debug(this.id + "._resetSubscriptions");
+      // Release handles on previous object, if any.
+      this.unsubscribeAll();
+
+      // When a mendix object exists create subscribtions.
+      if (this._contextObj && this.refreshAction) {
+        logger.debug(this.id + "._resetSubscriptions setup refresh handler");
+        this.subscribe({
+          guid: this._contextObj.getGuid(),
+          callback: dojoLang.hitch(this, function (guid) {
+            if (this._loadingStarted == false) {
+              logger.debug(this.id + ".Refresh triggered.");
+              this._pageInitiated = false;
+              this._updateRendering();
+            } else {
+              logger.debug(this.id + ".Refresh skip because of loading started.");
+            }
+          })
+        });
+      }
+    },
+
+    _execMf: function (mf, guid, cb) {
+      logger.debug(this.id + "._execMf" + (mf ? ": " + mf : ""));
+      if (mf && guid) {
+        mx.ui.action(mf, {
+          async: this.asyncCall,
+          params: {
+            applyto: "selection",
+            guids: [guid]
+          },
+          callback: (cb && typeof cb === "function" ? dojoLang.hitch(this, cb) : null),
+          error: function (error) {
+            logger.debug(error.description);
+          }
+        }, this);
+      }
+    },
+
+    _executeCallback: function (cb, from) {
+      logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+      if (cb && typeof cb === "function") {
+        cb();
+      }
+    }
+  });
 });
 
 require(["DataviewLoader/widget/DataviewLoader"]);
